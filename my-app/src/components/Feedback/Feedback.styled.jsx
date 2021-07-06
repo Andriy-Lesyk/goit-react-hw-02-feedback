cdfbfb1ea86f1737791fcc27873258b4fd5275ae
Andriy-Lesyk <74345435+Andriy-Lesyk@users.noreply.github.com>
2021-07-06 17:07:38 +0300

    переделывает дз

---
+++ my-app/src/components/Feedback/Feedback.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled"
+
+
+export const Title = styled.h2`
+
+`
+export const Btn = styled.button`
+margin-right: 10px;
+margin-left: 10px;
+`
+
+export const ContBtn = styled.div`
+`
+export const StatItem = styled.p`
+
+`
